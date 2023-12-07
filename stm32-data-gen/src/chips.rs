f3c498160c531c2806bc5e31f43a16f29123e0a4
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2023-12-06 18:44:40 -0600

    Add CORDIC v1 to STM32U5

--- stm32-data-gen/src/chips.rs
+++ stm32-data-gen/src/chips.rs
@@ -277,6 +277,7 @@ impl PeriMatcher {
             (".*:SAI:sai1_v2_1", ("sai", "v4", "SAI")),
             ("STM32U5.*:SAI[12]:.*", ("sai", "v4", "SAI")),
             ("STM32U5.*:ICACHE:.*", ("icache", "v1", "ICACHE")),
+            ("STM32U5.*:CORDIC:.*", ("cordic", "v1", "CORDIC")),
             (".*:SDIO:sdmmc_v1_2", ("sdmmc", "v1", "SDMMC")),
             (".*:SDMMC:sdmmc_v1_3", ("sdmmc", "v1", "SDMMC")),
             (".*:SPDIFRX:spdifrx1_v1_0", ("spdifrx", "v1", "SPDIFRX")),
